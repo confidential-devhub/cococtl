f289b9a75ec6eac2cbb8884ec0d31a35c087c2a6
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2026-01-31 19:28:24 +0100

    Revert "hack: fix namespace to default in sealed secret"
    This reverts commit dff529b72c76bfaeb788bf21335be3fb2c70a226.

--- pkg/secrets/converter.go
+++ pkg/secrets/converter.go
@@ -50,18 +50,17 @@ func ConvertSecrets(refs []SecretReference, inspectedKeys map[string]*SecretKeys
 	for _, ref := range refs {
 		// Determine which keys to convert and actual namespace
 		var keysToConvert []string
-		// var namespace string
-		namespace := "default"
+		var namespace string
 
 		if inspectedKeys != nil {
 			// Use inspected keys if available
 			if secretKeys, ok := inspectedKeys[ref.Name]; ok {
 				keysToConvert = secretKeys.Keys
-				// namespace = secretKeys.Namespace
+				namespace = secretKeys.Namespace
 			} else if len(ref.Keys) > 0 {
 				// Fall back to known keys
 				keysToConvert = ref.Keys
-				// namespace = ref.Namespace
+				namespace = ref.Namespace
 			} else {
 				// No keys available
 				nsInfo := "current context namespace"
@@ -74,7 +73,7 @@ func ConvertSecrets(refs []SecretReference, inspectedKeys map[string]*SecretKeys
 			// No inspection data - use only known keys
 			if len(ref.Keys) > 0 {
 				keysToConvert = ref.Keys
-				// namespace = ref.Namespace
+				namespace = ref.Namespace
 			} else {
 				nsInfo := "current context namespace"
 				if ref.Namespace != "" {
